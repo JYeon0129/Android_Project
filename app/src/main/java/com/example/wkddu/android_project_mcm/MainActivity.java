578cea6594f2f92898c1fc937bcaa68d3d7d7e2a
DESKTOP-23NVG7J\wkddu <wkddus119@naver.com>
2018-05-29 19:21:29 +0900

    프로젝트 시작 커밋

---
+++ app/src/main/java/com/example/wkddu/android_project_mcm/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.wkddu.android_project_mcm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
