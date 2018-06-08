c307a8c4d39e9b2f8cd69d36a35efb497efda3f8
soyoung <3457soso@naver.com>
2018-06-08 16:01:39 +0900

    #6 마무리

--- app/src/main/java/com/example/wkddu/android_project_mcm/TodoDetailFragment.java
+++ app/src/main/java/com/example/wkddu/android_project_mcm/TodoDetailFragment.java
@@ -25,7 +25,7 @@ import java.util.Date;
 
 public class TodoDetailFragment extends Fragment {
     TextView todoDetailTitleText, todoDetailCostText, todoDetailDateText,
-        todoDetailTypeText, todoDetailBalanceText, todoDetailAllowText;
+        todoDetailTypeText, todoDetailBalanceText, todoDetailAllowText, todoDetailAfterText;
     Button todoDetailUpdate, todoDetailDelete;
     ImageButton todoDetailBack;
     View todoDetailTypeView;
@@ -58,24 +58,19 @@ public class TodoDetailFragment extends Fragment {
         todoDetailTypeText = (TextView) getActivity().findViewById(R.id.todoDetailTypeText);
         todoDetailBalanceText = (TextView) getActivity().findViewById(R.id.todoDetailBalanceText);
         todoDetailAllowText = (TextView) getActivity().findViewById(R.id.todoDetailAllowText);
+        todoDetailAfterText = (TextView) getActivity().findViewById(R.id.todoDetailAfterText);
         todoDetailUpdate = (Button) getActivity().findViewById(R.id.todoDetailUpdate);
         todoDetailDelete = (Button) getActivity().findViewById(R.id.todoDetailDelete);
         todoDetailBack = (ImageButton) getActivity().findViewById(R.id.todoDetailBack);
         todoDetailTypeView = (View) getActivity().findViewById(R.id.todoDetailTypeView);
 
-        if (todoDetailTitleText != null) {
-            todoDetailTitleText.setText(todo.getTodoName());
-        }
+        todoDetailTitleText.setText(todo.getTodoName());
+        todoDetailCostText.setText(todo.getCost()+"원");
 
-        if (todoDetailCostText != null) {
-            todoDetailCostText.setText(todo.getCost()+"원");
-        }
+        Date current = todo.getDate();
+        String str = current.getYear() + "년 " + current.getMonth() + "월 " + current.getDay() + "일";
+        todoDetailDateText.setText(str);
 
-        if (todoDetailDateText != null) {
-            Date current = todo.getDate();
-            String str = current.getYear() + "년 " + current.getMonth() + "월 " + current.getDay() + "일";
-            todoDetailDateText.setText(str);
-        }
 
         /*
          * 이강민 : 밑에 뷰들에 값 채워줘야 해요!
@@ -83,27 +78,13 @@ public class TodoDetailFragment extends Fragment {
          * todoDetailTypeText : 타입의 이름 (술약속인지 밥약속인지..) 보여주는 뷰입니다
          * todoDetailBalanceText : 현재 잔고가 얼마인지 보여주는 뷰입니다
          * todoDetailAllowText : 오늘 하루 허용된 용돈이 얼마인지 보여주는 뷰입니다
+         * todoDetailAfterText : 해당 금액을 사용하고 난 뒤에 얼마가 남는지 보여주는 뷰입니다
          *
          * 그리고 버튼 눌렀을 때 액션도 추가해줘야 합니다. 리스터는 붙여뒀습니다!
          * todoDetailUpdate : 수정 버튼 클릭
          * todoDetailDelete : 삭제 버튼 클릭
          */
 
-        if (todoDetailTypeView != null) {
-
-        }
-
-        if (todoDetailTypeText != null) {
-
-        }
-
-        if (todoDetailBalanceText != null) {
-
-        }
-
-        if (todoDetailAllowText != null) {
-
-        }
 
         todoDetailUpdate.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
